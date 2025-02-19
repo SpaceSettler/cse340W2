e587293bc022d17ccdf706969cafdfffaa04effa
SpaceSettler <j.devon.richardson@gmail.com>
2025-02-19 02:46:23 -0700

    fixed an error to properly error

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -7,8 +7,6 @@ baseController.buildHome = async function (req, res) {
 }
 
 baseController.Error = async function (req, res) {
-    const nav = await utilites.getNav()
-    res.render("errors/error", {title: "Error Page", nav})
     throw new Error('500')
 }
 
--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else if (err.message == '500') {message = "Error 500"} else {message = 'Oh no! There was a a crash. Maybe try a different route?'}
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message, nav
