df3ffad7777279a2498602e6e42b57595329d0d4
SpaceSettler <j.devon.richardson@gmail.com>
2025-03-14 16:23:47 -0600

    What?

--- models/inventory-models.js
+++ models/inventory-models.js
@@ -16,7 +16,7 @@ async function getInventoryByClassificationId(classification_id) {
         const data = await pool.query(`SELECT * FROM public.inventory AS i
             JOIN public.classification AS c ON i.classification_id = c.classification_id
             WHERE i.classification_id = $1`,
-            [classification_id])
+            [classification_id]);
         return data.rows
     } catch (error) {
         console.error("getclassificationsbyid error ", + error)
